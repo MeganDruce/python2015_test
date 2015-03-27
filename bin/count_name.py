3365f7ea2a9566535332769b8e7d247a7367ea86
Megan Druce <megdruce26@gmail.com>
2015-03-27 11:14:10 +0200

    MeganTestAgain

--- bin/count_name.py
+++ bin/count_name.py
@@ -3,27 +3,17 @@
 import sys
 
 def count_name(filename, protein_name):
-#	input_file = open(filename)
-	count = 0
-	for line in input_file:
-		if line.rstrip() == protein_name:
-			count += 1
-	return count
+    input_file = open(filename)
+    count = 0
+    for line in input_file:
+        if line.rstrip() == protein_name:
+            count += 1
+    return count
 
 if len(sys.argv) != 3:
-	sys.exit("Usage: count_name.py <filename> <protein_name>")
+    sys.exit("Usage: count_name.py <filename> <protein_name>")
 
 filename = sys.argv[0]
-try:
-	input_file = open(filename)	
-
 protein_name = sys.argv[1]
-
-except IOError as e:
-	print >>sys.stderr, "couldn't open {}: {}".format(filename, e.strerror)
-	sys.exit(1)
-
-else:
-	count = count_name(filename, protein_name)
-	print protein_name, name_count
-
+count = count_name(filename, protein_name)
+print protein_name, name_count
